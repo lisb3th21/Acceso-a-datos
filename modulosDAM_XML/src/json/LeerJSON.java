be3e633184dd807d3f7b5c769e6cfd4bcfcf6a82
lisb3th21 <epoaquizacango@cifpfbmoll.eu>
2022-11-16 12:39:57 +0100

    lectura por terminal del json
    lectura del json terminada y comentada

--- modulosDAM_XML/src/json/LeerJSON.java
+++ modulosDAM_XML/src/json/LeerJSON.java
@@ -2,6 +2,7 @@ package json;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Set;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -11,31 +12,40 @@ public class LeerJSON {
 
     public static void main(String[] args) {
         try {
+
             String stringFicheroJSON = new String(Files.readAllBytes(Paths.get(ruta)));
-            System.out.println(stringFicheroJSON);
+            // Creo el objeto JSON
             JSONObject jsonObj = new JSONObject(stringFicheroJSON);
-
-            System.out.println("KeySet" + jsonObj.keySet());
-            System.out.println("Length " + jsonObj.length());
-
-            // String instiValue = jsonObj.getString("cicle");
-            // String value = jsonObj.ge
-
-            // System.out.println("Insituto " + instiValue);
-
-            JSONArray arrCicles = jsonObj.getJSONArray("cursos");
+            // Obtengo las claves raices: [segundo aÃ±o, cicle, primer aÃ±o]
+            Set<String> raiz = jsonObj.keySet();
             /*
-             * for (int i = 0; i < arrCicles.length(); i++) {
-             * System.out.println("Empleados " + arrCicles.get(i));
-             * System.out.println("Id: " + arrCicles.getJSONObject(i).get("id"));
-             * System.out.println("Apellido1: " +
-             * arrCicles.getJSONObject(i).get("llinatge1"));
-             * System.out.println("Apellido2: " +
-             * arrCicles.getJSONObject(i).get("llinatge2"));
-             * System.out.println("Nombre: " + arrCicles.getJSONObject(i).get("nom"));
-             * System.out.println("Telefono: " + arrCicles.getJSONObject(i).get("telefon"));
-             * }
-             *
+             * El el primer for recogo las claves externas y obtengo el objeto
+             */
+            for (String claves : raiz) {
+                System.out.println("-------------------");
+                System.out.println(claves.toUpperCase());
+                // ! hare una u otra cosa si es un JSONArray o un JSONObject
+                if (jsonObj.get(claves) instanceof JSONArray) {
+                    JSONArray arrCicles = jsonObj.getJSONArray(claves);
+                    int size = arrCicles.length();
+                    // recorro el jsonarray obteniendo cada uno de sus valores
+                    for (int i = 0; i < size; i++) {
+                        JSONObject kk = arrCicles.getJSONObject(i);
+                        Set<String> pp = kk.keySet();
+                        for (String string : pp) {
+                            System.out.println(string.toUpperCase() + ": " + kk.get(string));
+                        }
+                        System.out.println("");
+                    }
+                } else {
+                    // si es que es un jsonobject obtendre el objeto e imprimire sus atributos
+                    JSONObject obj = jsonObj.getJSONObject(claves);
+                    Set<String> keys = obj.keySet();
+                    for (String string : keys) {
+                        System.out.println(string.toUpperCase() + ": " + obj.get(string));
+                    }
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
